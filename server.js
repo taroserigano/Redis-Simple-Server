b5ee911be28613189a4cc7bfa1d92a7ab765123d
Taro Serigano <taroserigano@gmail.com>
2022-04-11 09:40:01 -0500

    init commit
    init commit

---
+++ server.js
@@ -0,0 +1,59 @@
+const express = require("express")
+const axios = require("axios")
+const cors = require("cors")
+const Redis = require("redis")
+const redisClient = Redis.createClient() 
+
+const DEFAULT_EXPIRATION = 3600
+
+const app = express() 
+app.use(express.urlencoded({ extended: true }))
+app.use(cors())
+
+app.get("/photos", async(req, res) => { 
+    const albumId = req.query.albumId
+    const photos = await getOrSetCache(`photos?albumId=${albumId}`, async()=> {
+            const { data} = await axios.get(
+                "https://jsonplaceholder.typicode.com/photos",
+            {params: {albumId}}
+            )
+                return data 
+                })
+
+        res.json(photos) 
+    })
+
+
+
+app.get("/photos/:id", async (req, res) => { 
+    const photo = await getOrSetCache(`photos:${req.params.id}`, async()=> {
+        const { data} = await axios.get(
+        `https://jsonplaceholder.typicode.com/photos/${req.params.id}`
+    )
+    return data
+})
+    res.json(data)
+})
+
+function getOrSetCache(key, cb) { 
+    return new Promise((resolve, reject) => { 
+        redisClient.get(key, async (error, data) => { 
+            if( error) return reject(error)
+            if(data!= null) return resolve(JSON.parse(data))
+            const fetchData = await cb() 
+            redisClient.setex(key, DEFAULT_EXPIRATION, JSON.stringify(freshData))
+            resolve(freshData) 
+        })
+    })
+}
+
+// app.get('/', (req, res) => {
+//     res.send('Hello World!')
+//   })
+
+
+// app.listen(3000, function() { 
+//     console.log("running 3000")
+// })
+
+
